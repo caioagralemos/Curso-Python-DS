fda03912fcd2f19c33ebc9289d435bd1910b99b2
caioagralemos <caioagralemos@live.com>
2023-06-18 15:47:33 -0300

    Update 65 - quicksort.py

--- 14 - Quick Sort/65 - quicksort.py
+++ 14 - Quick Sort/65 - quicksort.py
@@ -1,4 +1,17 @@
-import pivot
+def swap(my_list, index1, index2):
+    temp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = temp
+
+def pivot(lista, pivot_index):
+    swap_index = pivot_index
+    for i in range(pivot_index + 1, len(lista)):
+        if lista[i] < lista[pivot_index]:
+            # não entendi muito isso
+            swap_index += 1
+            swap(lista, swap_index, i)
+    swap(lista, swap_index, pivot_index)
+    return swap_index
 
 def quick_sort_helper(my_list, left, right):
     if left < right:
